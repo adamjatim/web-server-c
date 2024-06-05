336d04fb96cb3fd58e16eceaf1f1343aa1f34522
adamjatim <adamrahmatilahi@gmail.com>
2024-06-05 07:46:52 +0700

    init

---
+++ hdelibc.cpp
@@ -0,0 +1,13 @@
+// 
+// Homo Deus
+// 1/21/21
+// 
+// Homo Deus C++ Library
+// hdelibc.cpp
+// 
+// Adam Rahmat Ilahi
+// 
+
+
+
+#include "hdelibc.hpp"
---
+++ hdelibc.hpp
@@ -0,0 +1,18 @@
+// 
+// Homo Deus
+// 1/21/21
+// 
+// Homo Deus C++ Library
+// hdelibc.hpp
+// 
+// Adam Rahmat Ilahi
+// 
+
+
+
+#ifndef hdelibc_hpp
+#define hdelibc_hpp
+
+#include <stdio.h>
+
+#endif /* hdelibc_hpp */
